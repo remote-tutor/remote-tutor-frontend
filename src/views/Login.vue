54af107fb7cda33c60fc8fb86e09f194bb260f94
Mohamed Tawfik <motawfik1998@gmail.com>
2020-08-22 02:09:15 +0200

    Added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Username"
+                    name="username"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    :v-model="user.username"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    :append-icon="hiddenPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="hiddenPassword = !hiddenPassword"
+                    :type="hiddenPassword ? 'password' : 'text'"
+                    :v-model="user.password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="login" :loading="loading">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      hiddenPassword: true,
+      error: "",
+      loading: false,
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    async login() {},
+  },
+};
+</script>
