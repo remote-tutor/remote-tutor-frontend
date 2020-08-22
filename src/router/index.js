1b384ef3bb6a5267aaf1e139f8e1d3bffee7cec6
Mohamed Tawfik <motawfik98@outlook.com>
2020-08-22 02:09:15 +0200

    Added login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const DEFAULT_TITLE = "Remote Tutor"
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,6 +19,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      title: "Login"
+    }
   }
 ]
 
@@ -24,6 +34,12 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+  });
 })
 
 export default router
