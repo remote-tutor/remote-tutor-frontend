329bcc71d61e4821f807c4ca385ba15828e210d7
Mohamed Tawfik <motawfik1998@gmail.com>
2020-08-30 20:55:27 +0200

    Added delete question

--- src/components/announcements/shared/Announcement.vue
+++ src/components/announcements/shared/Announcement.vue
@@ -54,7 +54,7 @@
 
 <script>
 import api from "@/gateways/api.js";
-import ConfirmationDialog from "@/components/announcements/admins/ConfirmationDialog.vue";
+import ConfirmationDialog from "@/components/utils/ConfirmationDialog.vue";
 export default {
   name: "Announcement",
   components: {
