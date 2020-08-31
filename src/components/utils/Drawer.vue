f563d8211bdd448638af3cd564eb9d31cb1cfbf1
Mohamed Tawfik <motawfik98@outlook.com>
2020-08-31 15:32:40 +0200

    Added drawer

---
+++ src/components/utils/Drawer.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <template>
+      <v-navigation-drawer
+          :permanent="$vuetify.breakpoint.mdAndUp"
+          :expand-on-hover="$vuetify.breakpoint.mdAndUp"
+          dark
+          v-model="open"
+          app
+          clipped
+      >
+        <v-list>
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="title">User Full Name</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense rounded>
+          <v-list-item-group v-model="selected">
+            <v-list-item :to="{ name: 'Announcements' }">
+              <v-list-item-icon>
+                <v-icon>mdi-bullhorn</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Announcements</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: 'Quizzes' }">
+              <v-list-item-icon>
+                <v-icon>mdi-head-question</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Quizzes</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-comment-question</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Assignments</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-account-star</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Grades</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+      </v-navigation-drawer>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Drawer",
+  props: ["staticOpen"],
+  data() {
+    return {
+      open: this.staticOpen || false,
+      selected: 1,
+    }
+  },
+  watch: {
+    staticOpen: function (val) {
+      this.open = val
+    },
+    open: function (val) {
+      this.$emit('closedDrawer', {
+        value: val
+      })
+    }
+  }
+}
+</script>
