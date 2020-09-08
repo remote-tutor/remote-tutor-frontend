d75d83c09591342d7010c6743a579c43b93f4164
Mohamed Tawfik <motawfik98@outlook.com>
2020-09-08 21:06:36 +0200

    Features/admin (#6)
    * Implement pending users view to activate (as student or admin)
    
    * Added the ability for admin to update user data upon verification

--- src/components/students/admins/StudentsTable.vue
+++ src/components/students/admins/StudentsTable.vue
@@ -1,49 +1,84 @@
 <template>
   <v-card>
     <v-card-title>
-      Peding Students
+      Pending Students
       <v-spacer></v-spacer>
       <v-text-field
-        v-model="searchBy.value"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        clearable
-        @input="search"
+          v-model="searchBy.value"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          clearable
+          @input="search"
       ></v-text-field>
       <v-spacer></v-spacer>
       <v-select :items="searchByItems" v-model="searchBy.field" label="Search By" @input="search"></v-select>
     </v-card-title>
     <v-data-table
-      :headers="headers"
-      :items="students"
-      :options.sync="options"
-      :server-items-length="totalStudents"
-      :loading="loading"
-      :footer-props="{
+        :headers="headers"
+        :items="students"
+        :options.sync="options"
+        :server-items-length="totalStudents"
+        :loading="loading"
+        :footer-props="{
         'items-per-page-options': [10, 15, 20]
       }"
-      :items-per-page="10"
-      class="elevation-1"
+        :items-per-page="10"
+        class="elevation-1"
     >
+      <template v-slot:footer>
+        <v-btn block color="primary" @click="submitState">Submit</v-btn>
+      </template>
+
+      <template v-slot:item.full_name="props">
+        <v-edit-dialog
+            :return-value.sync="props.item.full_name"
+        > {{ props.item.full_name }}
+          <template v-slot:input>
+            <v-text-field
+                v-model="props.item.full_name"
+                label="Edit"
+                single-line
+                counter
+            ></v-text-field>
+          </template>
+        </v-edit-dialog>
+      </template>
+
+      <template v-slot:item.year="props">
+        <td :bgcolor="(props.item.year > 3 || props.item.year < 1) ? 'error' : ''">
+          <v-edit-dialog
+              :return-value.sync="props.item.year"
+          > {{ props.item.year }}
+            <template v-slot:input>
+              <v-text-field
+                  v-model="props.item.year"
+                  label="Edit"
+                  single-line
+                  counter
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </td>
+      </template>
+
       <template v-slot:item.created_at="{ item }">{{ item.created_at.substring(0, 10) }}</template>
       <template v-slot:item.status="{ item }">
         <v-row>
           <v-radio-group v-model="item.status" row>
-            <v-radio label="Admin" value="admin"></v-radio>
-            <v-radio label="Student" value="student"></v-radio>
-            <v-radio label="Decline" value="decline"></v-radio>
-            <v-btn color="primary" @click="submitState(item.id)">Submit</v-btn>
+            <v-radio label="Admin" value="1"></v-radio>
+            <v-radio label="Student" value="0"></v-radio>
+            <v-radio label="Decline" value="-1"></v-radio>
           </v-radio-group>
         </v-row>
       </template>
-      <template></template>
     </v-data-table>
   </v-card>
 </template>
 
 <script>
 import api from "@/gateways/api.js";
+
 export default {
   name: "StudentsTable",
   props: ["pending"],
@@ -53,16 +88,16 @@ export default {
       students: [],
       loading: false,
       options: {},
-      row: null,
       headers: [
-        { text: "Full Name", value: "full_name", width: "20%" },
-        { text: "Username", value: "username", width: "15%" },
-        { text: "Registered At", value: "created_at" },
-        { text: "State", value: "status", sortable: false },
+        {text: "Full Name", value: "full_name", width: "20%"},
+        {text: "Username", value: "username", width: "15%"},
+        {text: "Year", value: "year"},
+        {text: "Registered At", value: "created_at"},
+        {text: "State", value: "status", sortable: false},
       ],
       searchByItems: [
-        { text: "Username", value: "username" },
-        { text: "Full Name", value: "full_name" },
+        {text: "Username", value: "username"},
+        {text: "Full Name", value: "full_name"},
       ],
       searchBy: {
         value: "",
@@ -86,10 +121,10 @@ export default {
   methods: {
     getStudents() {
       this.loading = true;
-      const { sortBy, sortDesc, page, itemsPerPage } = this.options;
+      const {sortBy, sortDesc, page, itemsPerPage} = this.options;
       api({
         method: "GET",
-        url: "/students",
+        url: "/pending-students",
         params: {
           page: page,
           itemsPerPage: itemsPerPage,
@@ -100,42 +135,47 @@ export default {
           searchByField: this.searchBy.field,
         },
       })
-        .then((response) => {
-          this.students = response.data.students;
-          this.totalStudents = response.data.total;
-        })
-        .catch((error) => {
-          this.$store.dispatch("viewSnackbar", {
-            text: error.response.data.message,
-            color: "error",
+          .then((response) => {
+            this.students = response.data.pendingStudents;
+            this.totalStudents = response.data.totalPendingStudents;
+          })
+          .catch((error) => {
+            this.$store.dispatch("viewSnackbar", {
+              text: error.response.data.message,
+              color: "error",
+            });
+          })
+          .finally(() => {
+            this.loading = false;
           });
-        })
-        .finally(() => {
-          this.loading = false;
-        });
     },
     search() {
       this.options.page = 1;
       this.getStudents();
     },
-    submitState(id) {
-      let formData = new FormData();
-      formData.append("id", id);
-      api({
-        method: "PUT",
-        url: "/students",
-        data: formData,
+    submitState() {
+      this.sendState().then(() => {
+        this.students = []
+        this.getStudents()
       })
-        .then(() => {
-          this.getStudents();
-        })
-        .catch((error) => {
-          this.$store.dispatch("viewSnackbar", {
-            text: error.response.data.message,
-            color: "error",
-          });
-        });
+
     },
+    async sendState() {
+      for (let i = 0; i < this.students.length; i++) {
+        if (this.students[i].status === undefined)
+          continue
+        let formData = new FormData()
+        formData.append("userID", this.students[i].id)
+        formData.append("fullName", this.students[i].full_name)
+        formData.append("status", this.students[i].status)
+        formData.append("year", this.students[i].year)
+        await api({
+          method: "PUT",
+          url: "/students",
+          data: formData,
+        })
+      }
+    }
   },
 };
 </script>
